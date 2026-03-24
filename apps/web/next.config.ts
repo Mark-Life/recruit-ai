9dd24aa511b70fabc162d906f828d23f0e64db1c
Andrey Markin <108@mark-life.com>
2026-03-24 18:46:22 +0200

    Initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  transpilePackages: ["@workspace/ui", "@workspace/api", "@workspace/env"],
+  reactCompiler: true,
+  typedRoutes: true,
+  experimental: {
+    // Enable filesystem caching for `next dev`
+    turbopackFileSystemCacheForDev: true,
+    // Enable filesystem caching for `next build`
+    turbopackFileSystemCacheForBuild: true,
+  },
+};
+
+export default nextConfig;
